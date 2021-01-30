fdfbe9fac7c135b3eef888127e05eeffa42f9667
r-vu <46875289+r-vu@users.noreply.github.com>
2021-01-30 15:03:01 -0800

    Rename package and folders to be consistent
    Java packages must be in all lowercase to prevent conflict with
    interfaces and/or classes

--- src/main/java/fetchrewards/userpoints/UserPointsApplication.java
+++ src/main/java/fetchrewards/userpoints/UserPointsApplication.java
@@ -1,4 +1,4 @@
-package FetchRewards.UserPoints;
+package fetchrewards.userpoints;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

ec0cee163911700a62e6b5815bcc3e3b8b85d9a5
r-vu <46875289+r-vu@users.noreply.github.com>
2021-01-30 13:40:45 -0800

    Add project skeleton
    Project will be done in Java 8 using Spring

---
+++ src/main/java/FetchRewards/UserPoints/UserPointsApplication.java
@@ -0,0 +1,13 @@
+package FetchRewards.UserPoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FetchRewards/UserPoints/UserPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package FetchRewards.UserPoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

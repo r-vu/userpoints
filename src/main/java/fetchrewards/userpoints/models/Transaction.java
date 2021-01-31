683ff3c0c95c3631188242e53b9fd6dafb97d58b
r-vu <46875289+r-vu@users.noreply.github.com>
2021-01-30 23:58:38 -0800

    Add logic for adding and removing points

--- src/main/java/fetchrewards/userpoints/models/Transaction.java
+++ src/main/java/fetchrewards/userpoints/models/Transaction.java
@@ -4,8 +4,9 @@ package fetchrewards.userpoints.models;
 import java.time.*;
 
 public class Transaction {
+    public static final ZoneId UTC_TIMEZONE = ZoneOffset.UTC;
     private static long NEXT_ID = 1;
-    private static final ZoneId UTC_TIMEZONE = ZoneOffset.UTC;
+
     private final long transactionId;
     private final String payer;
     private final User recipient;
@@ -14,11 +15,58 @@ public class Transaction {
     private final LocalDateTime transactionDate;
 
     public Transaction(String payer, User recipient, int points) {
+        this(payer, recipient, points, LocalDateTime.now(UTC_TIMEZONE));
+    }
+
+    public Transaction(String payer, User recipient, int points, LocalDateTime time) {
         this.transactionId = NEXT_ID++;
         this.payer = payer;
         this.recipient = recipient;
         this.initialPoints = points;
         this.currentPoints = points;
-        this.transactionDate = LocalDateTime.now(UTC_TIMEZONE);
+        this.transactionDate = time;
+    }
+
+    public long getTransactionId() {
+        return this.transactionId;
+    }
+
+    public String getPayer() {
+        return this.payer;
+    }
+
+    public int getInitialPoints() {
+        return this.initialPoints;
+    }
+
+    public int getCurrentPoints() {
+        return this.currentPoints;
+    }
+
+    public boolean equalPayers(Transaction t) {
+        return this.payer.equals(t.payer);
+    }
+
+    /**
+     * Method to deduct points from this transaction and return a value
+     * to allow chaining deductions if required
+     *
+     * @param points Amount to deduct
+     * @return Returns the remainder needed to complete a deduction
+     */
+    public int deductPoints(int points) {
+        if (points < 0) {
+            return 0;
+        }
+
+        if (currentPoints >= points) {
+            currentPoints -= points;
+            return 0;
+        } else {
+            int remainder = points - currentPoints;
+            currentPoints = 0;
+            return remainder;
+        }
+
     }
 }
--- src/main/java/fetchrewards/userpoints/models/User.java
+++ src/main/java/fetchrewards/userpoints/models/User.java
@@ -1,20 +1,107 @@
 package fetchrewards.userpoints.models;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class User {
 
     private static long NEXT_ID = 1;
     private final long userId;
-    private Map<String, Integer> points;
-    private List<Transaction> transactions;
+    private Map<String, Integer> pointBreakdown;
+    private int balance;
+    private final List<Transaction> transactionList;
+    private final Deque<Transaction> transactionDeque;
 
     public User() {
         this.userId = NEXT_ID++;
-        this.points = new HashMap<>();
-        this.transactions = new ArrayList<>();
+        this.pointBreakdown = new HashMap<>();
+        this.balance = 0;
+
+        // Used to track all transactions in history
+        this.transactionList = new ArrayList<>();
+
+        // Used to serve qualified transactions for deductions
+        this.transactionDeque = new ArrayDeque<>();
+    }
+
+    public long getUserId() {
+        return this.userId;
+    }
+
+    public String getPointBreakdown() {
+        return null;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return this.transactionList;
+    }
+
+    public void addPoints(String payer, int points) {
+        Transaction t = new Transaction(payer, this, points);
+        addTransaction(t);
+    }
+
+    /**
+     * Method used to create ineligible transactions, clearing
+     * the transactionDeque
+     * @return A map of all the transactions created. Empty if operation
+     * failed
+     */
+    public Map<String, Integer> deductPoints(int points) {
+
+        Map<String, Integer> deductionHistory = new HashMap<>();
+
+        if (balance < points) {
+            return deductionHistory;
+        }
+
+
+        LocalDateTime timestamp = LocalDateTime.now(Transaction.UTC_TIMEZONE);
+
+        while (points > 0 && !transactionDeque.isEmpty()) {
+            Transaction current = transactionDeque.peekFirst();
+
+            Transaction next;
+            if (points >= current.getCurrentPoints()) {
+                next = new Transaction(
+                    current.getPayer(), this, -1 * current.getCurrentPoints(), timestamp);
+                addTransaction(next);
+                transactionDeque.removeFirst();
+                points -= current.getCurrentPoints();
+            } else {
+                next = new Transaction(
+                    current.getPayer(), this, -1 * (current.getCurrentPoints() - points), timestamp);
+                addTransaction(next);
+                points = 0;
+            }
+
+            deductionHistory.put(
+                next.getPayer(), deductionHistory.getOrDefault(next.getPayer(), 0) + next.getInitialPoints());
+        }
+
+        return deductionHistory;
+    }
+
+    private void addTransaction(Transaction t) {
+        if (t.getInitialPoints() > 0) {
+            transactionDeque.addLast(t);
+        } else if (t.getInitialPoints() < 0) {
+            for (Transaction transaction : transactionDeque) {
+                if (t.equalPayers(transaction)) {
+                    int remainder = transaction.deductPoints(-1 * t.getInitialPoints());
+                    if (remainder == 0) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        balance += t.getInitialPoints();
+        pointBreakdown.put(t.getPayer(), pointBreakdown.getOrDefault(t.getPayer(), 0) + t.getInitialPoints());
+        transactionList.add(t);
     }
 }

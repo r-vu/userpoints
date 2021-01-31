7939ec66efcf844ab13012e9ba690832c07c4834
r-vu <46875289+r-vu@users.noreply.github.com>
2021-01-31 10:27:15 -0800

    Add unit test for Transaction.equalPayer()
    Also renamed tests to follow naming conventions

--- src/test/java/fetchrewards/userpointstests/modeltests/TransactionTest.java
+++ src/test/java/fetchrewards/userpointstests/modeltests/TransactionTest.java
@@ -9,16 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TransactionTest {
 
     @Test
-    public void TestIncrementId() {
+    public void testIncrementId() {
         User user1 = new User();
         Transaction transaction1 = new Transaction("PAYER", user1, 100);
-        assertEquals(1, transaction1.getTransactionId());
         Transaction transaction2 = new Transaction("PAYER", user1, 200);
-        assertEquals(2, transaction2.getTransactionId());
+        assertEquals(transaction1.getTransactionId() + 1, transaction2.getTransactionId());
     }
 
     @Test
-    public void DeductionTest_SufficientPoints() {
+    public void testDeductPointsSufficientPoints() {
         User user1 = new User();
         Transaction transaction1 = new Transaction("PAYER", user1, 100);
         int result1 = transaction1.deductPoints(100);
@@ -26,10 +25,20 @@ public class TransactionTest {
     }
 
     @Test
-    public void DeductionTest_InsufficientPoints() {
+    public void testDeductPointsInsufficientPoints() {
         User user1 = new User();
         Transaction transaction1 = new Transaction("PAYER", user1, 100);
         int result1 = transaction1.deductPoints(101);
         assertEquals(1, result1);
+    }
+
+    @Test
+    public void testEqualPayers() {
+        User user1 = new User();
+        Transaction transaction1 = new Transaction("PAYER1", user1, 100);
+        Transaction transaction2 = new Transaction("PAYER1", user1, 200);
+        Transaction transaction3 = new Transaction("PAYER2", user1, 300);
+        assertTrue(transaction1.equalPayers(transaction2));
+        assertFalse(transaction2.equalPayers(transaction3));
     }
 }
